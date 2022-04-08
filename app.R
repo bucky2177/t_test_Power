4af9dbd4b70cad6eb447f491bd5feb4e4de17a3a
bucky2177 <v.koob@web.de>
2022-04-08 22:03:44 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,213 +1,165 @@
 ###############
 # Autor: Valentin Koob
+# Description: Shows the influence of assumption violations 
+# on the t-test
 ###############
 
-list.of.packages <- c("colorspace")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-library(colorspace)
+
+
+
 
 ######### Ab hier nur noch Berechnungen und Plots #########
-
-plotMeans = function(means, sigma.2) {
-  means = unlist(strsplit(means, "[ ]"))
-  means = as.numeric(means)
-  groups = 1:length(means)
-  sigma.2 = as.numeric(sigma.2)
+plotPopulation = function(sampleInput, 
+                          sdInput1,
+                          sdInput2){
   
-  plot(
-    means ~ groups,
-    ylim = c(min(means) - 2 - sqrt(sigma.2), max(means) + 2 + sqrt(sigma.2)),
-    type = "b",
-    axes = FALSE,
-    ylab = "Mittelwerte",
-    xlab = "Gruppen",
-    pch = 19
-  )
   
-  axis(side = 1,
-       at = groups)
-  axis(
-    side = 2,
-    las = 2,
-    at = seq(
-      from = min(means) - 4 - ceil(sqrt(sigma.2)),
-      to = max(means) + 4 + ceil(sqrt(sigma.2)),
-      by = 2.5
-    )
-  )
-  errbar(
-    x = groups,
-    y = means,
-    yplus = means + sqrt(sigma.2),
-    yminus = means - sqrt(sigma.2),
-    add = TRUE
-  )
-}
-
-plotNormalPops = function(means, sigma.2){
-  
-  means = unlist(strsplit(means, "[ ]"))
-  means = stringr::str_replace(means, ",", ".")
-  means = as.numeric(means)
-  groups = 1:length(means)
-  sigma.2 = unlist(strsplit(sigma.2, "[ ]"))
-  sigma.2 = stringr::str_replace(sigma.2, ",", ".")
-  sigma.2 = as.numeric(sigma.2)
-  if(length(sigma.2) >= 2){
-    warning("Mehr als eine Varianz spezifiziert")
-    return(NULL)
-  }
-  if(length(means) > 2){
-    warning("Mehr als zwei Mittelwerte spezifiziert")
-    return(NULL)
+  # find the min and max values of the population
+  if (sampleInput == "norm") {
+    xmin = floor(-max(sdInput1, sdInput2) * 4)
+    xmax = ceiling(-xmin)
+  } else if (sampleInput == "beta") { # varianz bei alpha = 2 = beta -> 0.05
+    scale_1 = sqrt(sdInput1^2/0.05)
+    scale_2 = sqrt(sdInput2^2/0.05)
+    xmax = ceiling(max(scale_1, scale_2)/2) # da spaeter auf 0 zentriert 
+    xmin = -xmax
+  } else if (sampleInput == "unif") { # Var(unif) = 1/12 [0,1]
+    scale_1 = sqrt(sdInput1^2/(1/12))
+    scale_2 = sqrt(sdInput2^2/(1/12))
+    xmax = ceiling(max(scale_1, scale_2)/2) # da spaeter auf 0 zentriert 
+    xmin = -xmax
   }
   
-  par(mfrow = c(1, 2))
+  # calculate density distributions
+  xs = seq(xmin, xmax, length.out = 10000)
+  if (sampleInput == "norm"){
+    dichte1 = dnorm(xs, mean = 0, sd = sdInput1)
+    dichte2 = dnorm(xs, mean = 0, sd = sdInput2)
+  } else if (sampleInput == "beta"){
+    dichte1 = dbeta(seq(0, 1, length.out = 10000), 2, 2)/(scale_1)
+    dichte2 = dbeta(seq(0, 1, length.out = 10000), 2, 2)/(scale_2)
+  } else if(sampleInput == "unif"){
+    dichte1 = dunif(xs, min = -scale_1/2, max = scale_1/2)
+    dichte2 = dunif(xs, min = -scale_2/2, max = scale_2/2)
+  }
   
-  plot(c(1,2) ~ 1, 
-       col = "white", 
-       xlab = "Werte in den Populationen", 
-       ylab = "Dichte", 
-       axes = FALSE, 
-       cex.lab = 1.2,
-       ylim = c(0, dnorm(means[1], mean = means[1], sd = sqrt(sigma.2)))*1.8,
-       xlim = c(floor(min(means) - 6 * sqrt(sigma.2)), ceiling(max(means) + 6*sqrt(sigma.2))))
-  
-  axis(side = 1)
-  axis(side = 2)
-  x = seq(floor(min(means) - 3 * sqrt(sigma.2)),  ceiling(max(means) + 3*sqrt(sigma.2)), 0.01)
-  for(i in 1:length(means)){
-    #segments(means[i], 0, means[i], dnorm(means[i], mean = means[i], sd = sqrt(sigma.2)), col = "gray")
-    dfs = dnorm(x, mean = means[i], sd = sqrt(sigma.2))
-    points(dfs~ x, ty = "l", 
-           col = rainbow_hcl(10)[i], lwd = 2)
-    polygon(c(min(x),x, max(x)), c(0, dfs, 0), col = adjustcolor(rainbow_hcl(10)[i], alpha.f=0.2), border = NA)
+  # plot the distributions
+  if(sampleInput == "norm" | sampleInput == "unif"){
+    plot(c(1,2) ~ c(1,1), xlim = c(xmin, xmax), ty = "l", col = "white",
+         xlab = "moegliche Werte x", 
+         ylab = "f(x)", ylim = c(0, max(c(dichte1, dichte2)) + max(c(dichte1, dichte2))*0.35),
+         cex.lab = 1.25, cex.axis = 1.25)
+    polygon(x = xs, y = dichte1, col = rgb(0,1,0, 0.2))
+    polygon(x = xs, y = dichte2, col = rgb(1,0,0, 0.2))
+  }else if(sampleInput == "beta"){
+    plot(c(1,2) ~ c(1,1), xlim = c(xmin, xmax), ty = "l", col = "white",
+         xlab = "moegliche Werte x", 
+         ylab = "f(x)", ylim = c(0, max(c(dichte1, dichte2)) + max(c(dichte1, dichte2))*0.35))
+    polygon(x = seq(0-scale_1/2, scale_1/2, length.out = 10000), 
+            y = dichte1, col = rgb(0,1,0, 0.2))
+    polygon(x = seq(0-scale_2/2, scale_2/2, length.out = 10000), 
+            y = dichte2, col = rgb(1,0,0, 0.2))
     
   }
   
+  legend("topright", legend = c("Population 1", "Population 2"), col = c(rgb(0,1,0, 0.8), rgb(1,0,0, 0.8)), pch = 15, 
+         cex = 1.25)
+  
+  
 }
 
+plotSimulation = function(sampleInput, 
+                          sdInput1,
+                          sdInput2, 
+                          n,
+                          nSim, 
+                          buttonState){
+  
+  if(!sampleInput %in% c("norm", "beta", "unif"))
+    stop("NOT DEFINED DISTRIBUTION")
+  
 
-
-plotDensities = function(ns, means, sigma.2, alpha) {
-  alpha = unlist(strsplit(alpha, "[ ]"))
-  alpha = as.numeric(alpha)[1]
-  if(length(alpha) >= 2){
-    warning("Mehr als ein alpha spezifiziert")
-    return(NULL)
-  }
-  means = unlist(strsplit(means, "[ ]"))
-  means = stringr::str_replace(means, ",", ".")
-  means = as.numeric(means)
+  buttonState = as.numeric(buttonState)
+  
+  # if buttonState > 0  run the simulation
+  if (buttonState > 0) {
+    
+    # place holder while the user is waiting
+    par(mar = c(0,0,0,0))
+    plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+    text(x = 0.5, y = 0.5, paste("Bitte warten :)"), 
+         cex = 1.6, col = "black")
+    
+    # run the simulation
+    res = 
+    lapply(1:nSim, function(oneRep){
+      # get the samples in one repetition
+      if (sampleInput == "norm") {
+        sample1 = rnorm(n, mean = 0, sd = sdInput1)
+        sample2 = rnorm(n, mean = 0, sd = sdInput2)
+      } else if (sampleInput == "beta") {
+        scale_1 = sqrt(sdInput1 ^ 2 / 0.05) # Streckung der Daten
+        scale_2 = sqrt(sdInput2 ^ 2 / 0.05) # Streckung der Daten
+        sample1 = (rbeta(n, 2, 2) - 0.5) * scale_1
+        sample2 = (rbeta(n, 2, 2) - 0.5) * scale_2
+      } else if (sampleInput == "unif") {
+        scale_1 = sqrt(sdInput1 ^ 2 / (1 / 12)) # obere Grenze finden
+        scale_2 = sqrt(sdInput2 ^ 2 / (1 / 12)) # obere Grenze finden
+        sample1 = runif(n, min = -scale_1 / 2, max = scale_1 / 2)
+        sample2 = runif(n, min = -scale_2 / 2, max = scale_2 / 2)
+      }    
+      t_obj <- t.test(x = sample1,
+                      y = sample2,
+                      alternative =  "two.sided",
+                      var.equal = TRUE) # t test
+      t <- t_obj$statistic # t wert
+      p <- t_obj$p.value # p wert
+      return(matrix(c(t, p), nrow = 1, ncol = 2)) # Rueckgabe
+    })
+    res = do.call("rbind", res)
+    
+    #### plotten der theoretischen t-Verteilung 
+    par(mar=c(5.1, 4.1, 4.1, 2.1))
+    dfs = (2*n) - 2
+    xmin = -5
+    xmax = 5
+    xs = seq(xmin, xmax, length.out = 10000)
+    dichteT = dt(xs, df = dfs)
+    # hist(res[,1], xlim = c(xmin, xmax), prob = TRUE, breaks = 100,
+    #      xlab = "moegliche t-Werte", 
+    #      ylab = "f(t)", ylim = c(0, max(dichteT) + max(dichteT)*0.3), 
+    #      main = "") 
+    plot(c(1,2) ~ c(1,1), col = "white", xlim = c(xmin, xmax),
+         xlab = "moegliche t-Werte", 
+         ylab = "f(t)", ylim = c(0, max(dichteT) + max(dichteT)*0.35), 
+         main = "", cex.lab = 1.25, cex.axis = 1.25) 
+    dens = density(res[,1])
+    polygon(x = dens$x, y = dens$y, col = "skyblue")
+    points(dichteT ~ xs, col = "red", ty = "l", lwd = 2)
+    legend("topright", legend = c(paste("theoretische t-Verteilung\ndf =", dfs)
+                                        , "simulierte t-Werte"), col = c("red", "skyblue"), 
+           lty = 1, cex = 1.25)
+    
+    nomAlpha = mean(res[,2] <= 0.05)
+    mtext(side = 3, paste("% Fehlentscheidungen\n", round(nomAlpha, 4)*100), cex = 1.5, line = 0.5)
+    t_krit = qt(0.975, dfs)
+    segments(x0 = -t_krit, y0 = 0, x1 = -t_krit, y1 = 0.1)
+    segments(x0 = t_krit, y0 = 0, x1 = t_krit, y1 = 0.1)
+    text(x = -t_krit, y = 0.15, expression(t[krit]), cex = 1.25)
+    text(x = t_krit, y = 0.15, expression(t[krit]), cex = 1.25)
+    #text(x = 0, y = 0.45, paste("t(", dfs, ")", sep = "" ), cex = 1.25, col = "red")
+    
+  }else{
+  
+  
+    # place holder while the user is waiting
+    par(mar = c(0,0,0,0))
+    plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+    text(x = 0.5, y = 0.5, paste("Bitte Simulation starten :)"), 
+         cex = 1.6, col = "black")
  
-  ns = unlist(strsplit(ns, "[ ]"))
-  if(length(ns) >= 2){
-    warning("Mehr als eine Stichprobengrößen spezifiziert")
-    return(NULL)
-  }
-  ns = as.numeric(ns)[1]
-  ns = rep(ns, 2)
-  
-  sigma.2 = unlist(strsplit(sigma.2, "[ ]"))
-  sigma.2 = stringr::str_replace(sigma.2, ",", ".")
-  sigma.2 = as.numeric(sigma.2)
-  if(length(sigma.2) >= 2){
-    warning("Mehr als eine Varianz spezifiziert")
-    return(NULL)
-  }
-  
-  par(mfrow = c(1, 2))
-  x = seq(-5, 10, 0.01)
-  dfs_null = dt(x, sum(ns) - 2)
-  plot(
-    x,
-    rep(1, length(x)),
-    col = "white",
-    ylim = c(0, 1),
-    ylab = "Dichte",
-    xlab = "t",
-    cex.lab = 1.2,
-    axes = FALSE,
-    main = "t-Verteilung unter der H0"
-  )
-  axis(side = 1)
-  axis(side = 2)
-  
-  x_quantil = qt(1 - alpha, sum(ns) - 2)
-  polygon(c(0, x[x < x_quantil], x_quantil),
-          c(0, dfs_null[x < x_quantil], 0),
-          col = "green3",
-          border = NA)
-  polygon(c(x_quantil, x[x > x_quantil], max(x)),
-          c(0, dfs_null[x > x_quantil], 0),
-          col = "red",
-          border = NA)
-  points(x, dfs_null, ty = "l", pch = 5)
-  
-  
-  segments(x_quantil, 0, x_quantil, .6)
-  t_krit = round(x_quantil, 2)
-  text(x = x_quantil, y = 0.65, bquote(t[krit] ~ "=" ~ .(t_krit)), cex = 1.5)
-  text(x_quantil + 0.5, .1, expression(alpha), cex = 1.5)
-  
-  
-  
-  plot(
-    x,
-    rep(1, length(x)),
-    col = "white",
-    ylim = c(0, 1),
-    ylab = "Dichte",
-    xlab = "t",
-    cex.lab = 1.2,
-    axes = FALSE,
-    main = "t-Verteilung gegeben der wahren Situation \n (= unter der H1) \n mit gestrichelter H0"
-  )
-  axis(side = 1)
-  axis(side = 2)
-  
-  ncp = ((means[2] - means[1]) / sqrt(sigma.2)) * sqrt(sum(ns)/4) #ncp (vgl. power.t.test)
-  dfs_h1 = dt(x, sum(ns) - 2, ncp)
-
-  polygon(
-    c(min(x), x[x < x_quantil], x_quantil),
-    c(0, dfs_h1[x < x_quantil], 0),
-    col = "orange",
-    border = NA,
-    lty = 0
-  )
-  polygon(
-    c(x_quantil, x[x > x_quantil], max(x)),
-    c(0, dfs_h1[x > x_quantil], 0),
-    col = "dodgerblue",
-    border = NA,
-    lty = 0
-  )
-  points(x,
-         dfs_h1,
-         ty = "l",
-         pch = 5,
-         col = "black")
-  segments(x_quantil, 0, x_quantil, .6)
-  text(x = x_quantil, y = 0.65, bquote(t[krit] ~ "=" ~ .(t_krit)), cex = 1.5)
-  points(x,
-         dfs_null,
-         ty = "l",
-         pch = 5,
-         lty = 3)
-  
-  
-  ##Griechische Buchstaben
-  text(x_quantil - .9, .1, expression(beta), cex = 1.5)
-  text(x_quantil + 1, .1, expression(1 - beta), cex = 1.5)
-  
-  
-  ##Power
-  power = 1 - pt(x_quantil, sum(ns) - 2, ncp)
-  mtext(paste("Power = ", round(power, 2)), side=1, line=-15, at=8, cex = 1.5)
+  } 
 }
-
 
 
 
@@ -215,32 +167,50 @@ plotDensities = function(ns, means, sigma.2, alpha) {
 
 
 ui <- fluidPage(
-  titlePanel("Power beim t-Test (unabh. Stichproben)"),
+  titlePanel("Auswirkung der Annahmensverletzungen auf das nominelle Alpha-Niveau (beim t-Test fuer 
+             unabhaengige Stichproben"),
   sidebarLayout(
+    
     sidebarPanel(
-      textInput(
-        "MittelwerteInput",
-        h3("Erwartungswerte der Populationen"),
-        value = "1 1.5"
-      ),
-      textInput(
-        "Sigma.2Input",
-        h3("Varianz innerhalb der Populationen"),
-        value = "1"
-      ),
-      textInput("nsInput", h3("n pro Gruppe"),
-                value = "10"),
-      textInput("alphaInput", h3("alpha (Fehler 1. Art)"),
-                value = "0.05")
+      
+      sliderInput(
+        "SdInput1",
+        h4("Standardabweichung der 1. Population"),
+        min = 5, max = 50, value = 5, step = 5),
+      sliderInput(
+        "SdInput2",
+        h4("Standardabweichung der 2. Population"),
+        min = 5, max = 50, value = 5, step = 5),
+      sliderInput(
+        "nsInput",
+        h4("Stichprobengroeße"),
+        min = 3, max = 15, value = 7, step = 1),
+      selectInput(
+        "SampleInput",
+        h4("Woraus soll gezogen werden?"),
+        list(Normalverteilung = "norm", 
+             `skalierte Beta-Verteilung` = "beta", 
+             Gleichverteilung = "unif"),
+        "Normalverteilung"),
+      sliderInput(
+        "nSimInput",
+        h4("Wie oft soll gezogen werden? (Dauer der Simulation)"),
+        min = 5000, max = 15000, value = 5000, step = 5000),
+      h4("Simulation starten"),
+      actionButton(
+        "buttonInput", 
+        "go!")
+      
+
     ),
     
     mainPanel(fluidPage(
-      h3(
-        "Veranschaulichung der wahren Situation auf Populationsebene"
-      ),
-      fluidRow(plotOutput(outputId = "means")),
-      h3("Veranschaulichung der t-Verteilungen"),
-      fluidRow(plotOutput(outputId = "densities")),
+      
+      h4("Veranschaulichung der Populationen aus denen gezogen wird (unter der H0)"),
+      fluidRow(plotOutput(outputId = "population")),
+      
+      h4("Theoretische vs. simulierte Verteilung der t-Werte"),
+      fluidRow(plotOutput(outputId = "sim")),
       
     ))
   )
@@ -250,15 +220,19 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  output$densities <- renderPlot({
-    plotDensities(input$nsInput,
-                  input$MittelwerteInput,
-                  input$Sigma.2Input,
-                  input$alphaInput)
+  output$population <- renderPlot({
+    plotPopulation(input$SampleInput, 
+                   input$SdInput1,
+                   input$SdInput2)
   })
   
-  output$means <- renderPlot({
-    plotNormalPops(input$MittelwerteInput, input$Sigma.2Input)
+  output$sim <- renderPlot({
+    plotSimulation(input$SampleInput, 
+                   input$SdInput1,
+                   input$SdInput2, 
+                   input$nsInput,
+                   input$nSimInput, 
+                   input$buttonInput)
   })
   
 }
